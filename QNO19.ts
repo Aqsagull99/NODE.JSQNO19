7efaacaacd614ecfe7fe0ea09c0c0ce83b246f42
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:23:53 +0500

    Add files via upload

---
+++ QNO19.ts
@@ -0,0 +1,7 @@
+//  QNO:19
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+ 
+//  let numguest:string[]=["qirat","kashaf","shafaq","furqan"]
+
+// console.log(`you are invited ${numguest [1]}  to dinner.`)
